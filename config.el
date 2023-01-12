79ff7a588fe841ee2bf08d057056a01d40048e55
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2023-01-12 13:57:29 +0900

    move eyebrowse into global

--- config.el
+++ config.el
@@ -197,26 +197,24 @@
       ;; iedit
       "C-c C-e" #'iedit-mode
       ;; undo-tree
-      "C-z" #'undo-tree-visualize)
+      "C-z" #'undo-tree-visualize
+      ;; eyebrowse
+      "M-0" #'eyebrowse-switch-to-window-config-0
+      "M-1" #'eyebrowse-switch-to-window-config-1
+      "M-2" #'eyebrowse-switch-to-window-config-2
+      "M-3" #'eyebrowse-switch-to-window-config-3
+      "M-4" #'eyebrowse-switch-to-window-config-4
+      "M-5" #'eyebrowse-switch-to-window-config-5
+      "M-6" #'eyebrowse-switch-to-window-config-6
+      "M-7" #'eyebrowse-switch-to-window-config-7
+      "M-8" #'eyebrowse-switch-to-window-config-8
+      "M-9" #'eyebrowse-switch-to-window-config-9)
 
 (map! :after copilot
       :map copilot-completion-map
       "<tab>" #'copilot-accept-completion
       "TAB" #'copilot-accept-completion)
 
-(after! eyebrowse
-  (eyebrowse-mode t)
-  (map! :map eyebrowse-mode-map
-        "M-0" #'eyebrowse-switch-to-window-config-0
-        "M-1" #'eyebrowse-switch-to-window-config-1
-        "M-2" #'eyebrowse-switch-to-window-config-2
-        "M-3" #'eyebrowse-switch-to-window-config-3
-        "M-4" #'eyebrowse-switch-to-window-config-4
-        "M-5" #'eyebrowse-switch-to-window-config-5
-        "M-6" #'eyebrowse-switch-to-window-config-6
-        "M-7" #'eyebrowse-switch-to-window-config-7
-        "M-8" #'eyebrowse-switch-to-window-config-8
-        "M-9" #'eyebrowse-switch-to-window-config-9))
 
 ;;
 ;; kernel hooks
