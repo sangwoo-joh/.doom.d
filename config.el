19496963de5d00920bd1a5e2260604e3c11f6bca
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2023-01-27 23:52:55 +0900

    Change macos theme

--- config.el
+++ config.el
@@ -32,11 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(if IS-LINUX
-    (setq doom-theme 'doom-one))
-
-(if IS-MAC
-    (setq doom-theme 'doom-one-light))
+(setq doom-theme 'doom-one)
 
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
