aea3e7245047f43b4a52dc1f5a9215fce80bd6e0
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2024-05-22 17:56:08 +0900

    change org level colors

--- config.el
+++ config.el
@@ -701,8 +701,8 @@
   (custom-set-faces!
     '(org-level-1 :height 1.4 :weight extrabold :slant normal :foreground "cyan")
     '(org-level-2 :height 1.3 :weight bold :slant normal :foreground "violet")
-    '(org-level-3 :height 1.2 :weight regular :slant normal :foreground "magenta")
-    '(org-level-4 :height 1.1 :weight regular :slant normal :foreground "teal")
+    '(org-level-3 :height 1.2 :weight regular :slant normal :foreground "yellow")
+    '(org-level-4 :height 1.1 :weight regular :slant normal :foreground "orange")
     '(org-document-title
       :family "Roboto"
       :height 250
