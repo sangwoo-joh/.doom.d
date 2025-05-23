7d1617c3f4c2d7059026a9905610a166ceef8dbd
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2025-05-23 11:33:10 +0100

    edit org set faces

--- config.el
+++ config.el
@@ -808,10 +808,10 @@
     "Red."
     :group 'basic-faces)
   (custom-set-faces!
-    '(org-level-1 :height 2.0 :weight extrabold :slant normal)
-    '(org-level-2 :height 1.8 :weight bold :slant normal)
-    '(org-level-3 :height 1.5 :weight regular :slant normal)
-    '(org-level-4 :height 1.4 :weight regular :slant normal)
+    '(org-level-1 :height 2.5 :weight extrabold :slant normal)
+    '(org-level-2 :height 2.0 :weight bold :slant normal)
+    '(org-level-3 :height 1.8 :weight regular :slant normal)
+    '(org-level-4 :height 1.5 :weight regular :slant normal)
     '(org-level-5 :height 1.3)
     '(org-level-6 :height 1.2)
     '(org-level-7 :height 1.15)
